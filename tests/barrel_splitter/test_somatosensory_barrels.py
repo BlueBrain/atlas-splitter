4a8bfff856afca37efb55970f3d769cad64c8911
Aleksandra Zuzanna Teska <aleksandra.teska@epfl.ch>
2022-10-05 16:10:59 +0200

    barrel-splitter added, test suit, fixed docs

---
+++ tests/barrel_splitter/test_somatosensory_barrels.py
@@ -0,0 +1,22 @@
+import pytest
+import atlas_splitter.barrel_splitter.somatosensory_barrels as tested
+
+
+def test_get_hierarchy_by_acronym():
+    pass
+
+
+def test_positions_to_mask():
+    pass
+
+
+def test_edit_hierarchy():
+    pass
+
+
+def test_edit_volume():
+    pass
+
+
+def test_split_barrels():
+    pass
