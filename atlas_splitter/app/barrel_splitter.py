b4f727c51ed8cf3b9752157a3f07c71153bcc60f
Aleksandra Zuzanna Teska <aleksandra.teska@epfl.ch>
2022-10-05 14:44:18 +0200

    add docs, fix name

--- atlas_splitter/app/barrel_splitter.py
+++ atlas_splitter/app/barrel_splitter.py
@@ -1,3 +1,4 @@
+import os
 import json
 import voxcell
 import pandas as pd
@@ -5,25 +6,23 @@ import pandas as pd
 from atlas_splitter.barrel_splitter import somatosensory_barrels
 from voxcell.nexus.voxelbrain import Atlas
 
-annotation_path = "/gpfs/bbp.cscs.ch/project/proj100/atlas_work/mouse/atlases/20210531/brain_regions.nrrd"
-annotation = voxcell.VoxelData.load_nrrd(annotation_path)
+atlas_path = "/gpfs/bbp.cscs.ch/data/project/proj100/atlas_work/mouse/atlases/20210531/"
 
 barrel_positions = pd.read_feather(
     "/gpfs/bbp.cscs.ch/project/proj100/atlas_work/mouse/atlas_mouse/data/fixed_annotated_barrels.f"
 )
 
-atlas = Atlas.open(
-    "/gpfs/bbp.cscs.ch/data/project/proj100/atlas_work/mouse/atlases/20210531/"
-)
+atlas = Atlas.open(atlas_path)
 
-hierarchy_path = (
-    "/gpfs/bbp.cscs.ch/project/proj100/atlas_work/mouse/atlases/20210531/hierarchy.json"
-)
+annotation_path = os.path.join(atlas_path, "brain_regions.nrrd")
+annotation = voxcell.VoxelData.load_nrrd(annotation_path)
+
+hierarchy_path = os.path.join(atlas_path, "hierarchy.json")
 
 with open(hierarchy_path, "r", encoding="utf-8") as h_file:
     hierarchy = json.load(h_file)
 
-somatosensory_barrels.split(hierarchy, annotation.raw, barrel_positions, atlas)
+somatosensory_barrels.split_barrels(hierarchy, annotation.raw, barrel_positions, atlas)
 
 
 output_hierarchy_path = "/gpfs/bbp.cscs.ch/project/proj100/atlas_work/mouse/atlases/modification/hierarchy.json"
--- atlas_splitter/barrel_splitter/somatosensory_barrels.py
+++ atlas_splitter/barrel_splitter/somatosensory_barrels.py
@@ -63,7 +63,17 @@ def create_id_generator(region_map: RegionMap) -> Iterator[int]:
     return count(start=last + 1)
 
 
-def get_hierarchy_by_acronym(hierarchy, acronym):  
+def get_hierarchy_by_acronym(hierarchy, acronym):
+    """Find and retunr child with a matching acronym from the next level
+    of the hierarchy.
+
+    Args:
+        hierarchy (HierarchyDict): brain regions hierarchy dict
+        acronym (str): name of the region
+
+    Returns:
+        HierarchyDict: HierarchyDict of the matching child
+    """
     for index, child in enumerate(hierarchy["children"]):
         if child["acronym"] == acronym:
             return hierarchy["children"][index]
@@ -74,12 +84,12 @@ def positions_to_mask(positions, mask, orientation) -> BoolArray:
 
     Args:
         positions (np.array): 2D : x,y,z positions
-        mask (BoolArray): mask sample to reproduce the dimensions 
-        orientation (VoxelData): _description_
+        mask (BoolArray): mask sample to reproduce the dimensions
+        orientation (VoxelData): orientation field of the atlas
 
     Returns:
         BoolArray: mask of the region described by the positions
-    """    
+    """
     mask = np.zeros_like(mask)
     indices = orientation.positions_to_indices(positions)
 
@@ -94,8 +104,19 @@ def edit_hierarchy(
     new_ids: Dict[int, Dict[str, int]],
     region_map: RegionMap,
     start_index: int,
-    barrel_names,
+    children_names,
 ) -> None:
+    """Edit in place the hierarchy to include new children volumes of a given
+    region. Implemented to integrated the barrels into layer 4 of the Barrel
+    cortex in Somatosensory cortex.
+
+    Args:
+        hierarchy (HierarchyDict): brain regions hierarchy dict
+        new_ids (Dict[int, Dict[str, int]]): set of new ids
+        region_map (RegionMap):  map to navigate the brain regions hierarchy.
+        start_index (int): index of the starting brain region (Isocortex)
+        children_names (list): list of children volumes to be integrated
+    """
 
     hierarchy_levels = np.array(
         region_map.get(start_index, attr="acronym", with_ascendants=True)
@@ -108,7 +129,7 @@ def edit_hierarchy(
         hierarchy_ = get_hierarchy_by_acronym(hierarchy_, acronym)
 
     new_children = []
-    for name in barrel_names:
+    for name in children_names:
         new_child = copy.deepcopy(hierarchy_)
         new_child["parent_structure_id"] = hierarchy_["id"]
         new_child["acronym"] = hierarchy_["acronym"] + f"-{name}"
@@ -129,6 +150,16 @@ def edit_volume(
     region_id: int,
     new_ids: Dict[str, int],
 ):
+    """Edit in place layer 4 `volume` to incorporate children volumes of barrels.
+
+    Args:
+        volume (NDArray): whole brain annotated volume.
+        barrel_positions (pd.DataFrame): x,y,z voxel positions
+        atlas (voxelbrain.LocalAtlas): _description_
+        region (str): name of the parent region
+        region_id (int): parent region id
+        new_ids (Dict[str, int]): set of new ids
+    """
     mask = atlas.get_region_mask(region).raw
     orientation = atlas.load_data("orientation")
 
@@ -142,12 +173,26 @@ def edit_volume(
         volume[change_to_layer] = new_id
 
 
-def split(
+def split_barrels(
     hierarchy: HierarchyDict,
     annotation: VoxelData,
     barrel_positions: pd.DataFrame,
     atlas,
 ):
+    """
+    Introduces the barrels specified by a DataFrame to layer 4 of
+    barrel cortex region in place.
+
+    The `hierarchy` dict and the `annotation` are modified in-place.
+    All of the barrels present in the DF are introduced based on their
+    x, y, z voxel positions.
+
+    Args:
+        hierarchy (HierarchyDict): brain regions hierarchy dict
+        annotation (VoxelData): whole brain annotation
+        barrel_positions (pd.DataFrame): x,y,z voxel positions
+        atlas (voxelbrain.LocalAtlas): brain atlas
+    """
     barrel_names = np.sort(barrel_positions.barrel.unique())
     region_map = RegionMap.from_dict(hierarchy["msg"][0])
     indices = region_map.find("SSp-bfd4", attr="acronym", with_descendants=True)
@@ -155,6 +200,7 @@ def split(
     id_generator = create_id_generator(region_map)
 
     new_ids: Dict[int, Dict[str, int]] = defaultdict(dict)
+
     for name in barrel_names:
         new_ids[name] = next(id_generator)
 
@@ -162,8 +208,8 @@ def split(
         hierarchy,
         new_ids,
         region_map,
-        start_index=ssp_bfd4_index,
-        barrel_names=barrel_names,
+        ssp_bfd4_index,
+        barrel_names,
     )
 
     edit_volume(
