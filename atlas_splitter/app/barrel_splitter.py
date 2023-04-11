e8f7c615a4fc9549d4bc4c4b16d42bd80f6c2828
Aleksandra Zuzanna Teska <aleksandra.teska@epfl.ch>
2023-04-11 15:22:36 +0200

    update code for the barrel columns splitter

--- atlas_splitter/app/barrel_splitter.py
+++ atlas_splitter/app/barrel_splitter.py
@@ -1,11 +1,10 @@
 import json
 import logging
 import click
-import voxcell
 import pandas as pd
 
 from atlas_splitter.barrel_splitter import somatosensory_barrels
-from voxcell.nexus.voxelbrain import Atlas
+from voxcell import VoxelData
 from atlas_commons.app_utils import (
     EXISTING_FILE_PATH,
     common_atlas_options,
@@ -27,11 +26,6 @@ def app():
 @verbose_option
 @common_atlas_options
 @click.option(
-    "--atlas-path",
-    required=True,
-    help=("Path to the mouse isocortex Atlas directory."),
-)
-@click.option(
     "--barrels-path",
     type=EXISTING_FILE_PATH,
     required=True,
@@ -44,17 +38,16 @@ def app():
     "--output-annotation-path", required=True, help="Path of the nrrd file to write"
 )
 @log_args(L)
-def split_barrels(
+def split_barrel_columns(
     verbose,
     annotation_path,
     hierarchy_path,
-    atlas_path,
     barrels_path,
     output_hierarchy_path,
     output_annotation_path,
 ):
-    """Introduce the barrels to layer 4 of barrel cortex in the mouse atlas
-    annotation in place. Positions of the voxels need to be specified by a DataFrame.
+    """Introduce the barrel columns to SSp-bfd (primary somatosensory barrel cortex)
+    in the mouse annotations in place. Positions of the voxels need to be specified by a DataFrame.
 
     The `hierarchy` dict and the `annotation` are modified in-place.
     All of the barrels present in the DF are introduced based on their
@@ -67,17 +60,13 @@ def split_barrels(
 
     L.info("Loading files ...")
     barrel_positions = pd.read_feather(barrels_path)
-    atlas = Atlas.open(atlas_path)
-
-    annotation = voxcell.VoxelData.load_nrrd(annotation_path)
+    annotation = VoxelData.load_nrrd(annotation_path)
 
     with open(hierarchy_path, "r", encoding="utf-8") as h_file:
         hierarchy = json.load(h_file)
 
-    L.info("Introduce the barrels to layer 4...")
-    somatosensory_barrels.split_barrels(
-        hierarchy, annotation.raw, barrel_positions, atlas
-    )
+    L.info("Introduce the barrel columns to SSp-bfd...")
+    somatosensory_barrels.split_barrels(hierarchy, annotation, barrel_positions)
 
     L.info("Saving modified hierarchy and annotation files ...")
     with open(output_hierarchy_path, "w", encoding="utf-8") as out:
