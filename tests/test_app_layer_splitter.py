2dbe728ce8afbf1bc2c81079b48291184b9ffbd5
Aleksandra Zuzanna Teska <aleksandra.teska@epfl.ch>
2023-04-25 16:07:16 +0200

    test cli

--- tests/test_app_layer_splitter.py
+++ tests/test_app_layer_splitter.py
@@ -5,7 +5,7 @@ from atlas_commons.app_utils import assert_properties
 from click.testing import CliRunner
 from voxcell import RegionMap, VoxelData  # type: ignore
 
-import atlas_splitter.app as tested
+import atlas_splitter.app.cli as tested
 from tests.layer_splitter.utils import LAYER_23_IDS, get_splitting_input_data
 
 TEST_PATH = Path(__file__).parent
